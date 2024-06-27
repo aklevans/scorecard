b9f12ff971954533a20a1aa366984d2bcc26ee6f
aklevans <alexklevans@gmail.com>
2024-06-27 16:06:48 -0400

    Add tests for new deps.dev api calls
    Signed-off-by: aklevans <alexklevans@gmail.com>

--- e2e/depsdev_test.go
+++ e2e/depsdev_test.go
@@ -53,3 +53,103 @@ var _ = Describe("E2E TEST: depsdevclient.GetProjectPackageVersions", func() {
 		})
 	})
 })
+
+var _ = Describe("E2E TEST: depsdevclient.GetPackage", func() {
+	var client packageclient.ProjectPackageClient
+
+	Context("E2E TEST: Confirm GetPackage works", func() {
+		It("Should receive a non-empty response from deps.dev for existing projects", func() {
+			client = packageclient.CreateDepsDevClient()
+			versions, err := client.GetPackage(
+				context.Background(), "github.com", "ossf/scorecard", "GO",
+			)
+			Expect(err).Should(BeNil())
+			Expect(len(versions.Versions)).Should(BeNumerically(">", 0))
+		})
+		It("Should error from deps.dev for nonexistent projects", func() {
+			client = packageclient.CreateDepsDevClient()
+			versions, err := client.GetPackage(
+				context.Background(), "github.com", "ossf/scorecard-E2E-TEST-DOES-NOT-EXIST", "GO",
+			)
+			Expect(err).ShouldNot(BeNil())
+			Expect(versions).Should(BeNil())
+		})
+	})
+})
+
+var _ = Describe("E2E TEST: depsdevclient.GetPackageDependencies", func() {
+	var client packageclient.ProjectPackageClient
+
+	Context("E2E TEST: Confirm GetPackageDependencies works", func() {
+		It("Should receive a non-empty response from deps.dev for existing projects", func() {
+			client = packageclient.CreateDepsDevClient()
+			dependencies, err := client.GetPackageDependencies(
+				context.Background(), "github.com", "ossf/scorecard",
+			)
+			Expect(err).Should(BeNil())
+			Expect(len(dependencies.Nodes)).Should(BeNumerically(">", 0))
+		})
+		It("Should error from deps.dev for nonexistent projects", func() {
+			client = packageclient.CreateDepsDevClient()
+			versions, err := client.GetPackageDependencies(
+				context.Background(), "github.com", "ossf/scorecard-E2E-TEST-DOES-NOT-EXIST",
+			)
+			Expect(err).ShouldNot(BeNil())
+			Expect(versions).Should(BeNil())
+		})
+	})
+})
+
+var _ = Describe("E2E TEST: depsdevclient.GetVersion", func() {
+	var client packageclient.ProjectPackageClient
+
+	Context("E2E TEST: Confirm ProjectPackageClient works", func() {
+		It("Should receive a non-empty response from deps.dev for existing projects", func() {
+			client = packageclient.CreateDepsDevClient()
+			versionData, err := client.GetVersion(
+				context.Background(), "github.com/ossf/scorecard", "v1.2.0", "GO",
+			)
+			Expect(err).Should(BeNil())
+			Expect(versionData.VersionKey.Version).Should(Equal("v1.2.0"))
+		})
+		It("Should error from deps.dev for nonexistent projects", func() {
+			client = packageclient.CreateDepsDevClient()
+			versions, err := client.GetVersion(
+				context.Background(), "github.com/ossf/scorecard-E2E-TEST-DOES-NOT-EXIST", "v2.4.3", "GO",
+			)
+			Expect(err).ShouldNot(BeNil())
+			Expect(versions).Should(BeNil())
+		})
+	})
+})
+
+var _ = Describe("E2E TEST: depsdevclient.GetURI", func() {
+	var client packageclient.ProjectPackageClient
+
+	Context("E2E TEST: Confirm GetURI works", func() {
+		It("Should error from deps.dev for nonexistent projects", func() {
+			client = packageclient.CreateDepsDevClient()
+			URI, err := client.GetURI(
+				context.Background(), "github.com/ossf/scorecard-E2E-TEST-DOES-NOT-EXIST", "v2.4.3", "GO",
+			)
+			Expect(err).ShouldNot(BeNil())
+			Expect(URI).Should(Equal(""))
+		})
+		It("Should receive a non-empty response from deps.dev for existing projects", func() {
+			client = packageclient.CreateDepsDevClient()
+			URI, err := client.GetURI(
+				context.Background(), "@colors/colors", "1.5.0", "NPM",
+			)
+			Expect(err).Should(BeNil())
+			Expect(URI).Should(Equal("github.com/DABH/colors.js"))
+		})
+		It("Should error from deps.dev for non-github url", func() {
+			client = packageclient.CreateDepsDevClient()
+			URI, err := client.GetURI(
+				context.Background(), "golang.org/x/crypto", "v0.24.0", "GO",
+			)
+			Expect(err).ShouldNot(BeNil())
+			Expect(URI).Should(Equal(""))
+		})
+	})
+})
